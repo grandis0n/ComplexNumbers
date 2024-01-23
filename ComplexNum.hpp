517e3b710559c753cb2a5d25bc153bfc99e8ec7d
Ilya Bizimov <ilya.bizimov@mail.ru>
2024-01-24 00:48:33 +1000

    add concept, template and casts

--- ComplexNum.hpp
+++ ComplexNum.hpp
@@ -1,47 +1,155 @@
 #ifndef COMPLEX_COMPLEXNUM_HPP
 #define COMPLEX_COMPLEXNUM_HPP
+#include <cmath>
+#include <stdexcept>
+#include <iostream>
+#include <limits>
+#include <concepts>
+#include <type_traits>
 
+template <typename T>
+concept Arithmetic = std::is_arithmetic_v<T>;
+
+template <Arithmetic T>
 class ComplexNumber
 {
 private:
-    double real;
-    double imaginary;
+    T real;
+    T imaginary;
 
 public:
-    explicit ComplexNumber(double real = 0.0, double imaginary = 0.0);
+    ComplexNumber(T real, T imaginary)
+        : real(real), imaginary(imaginary)
+    {
+    }
 
-    double getReal() const;
+    T getReal() const
+    {
+        return real;
+    }
 
-    void setReal(double real);
+    void setReal(T value)
+    {
+        this->real = value;
+    }
 
-    double getImaginary() const;
+    T getImaginary() const
+    {
+        return imaginary;
+    }
 
-    void setImaginary(double imaginary);
+    void setImaginary(T value)
+    {
+        this->imaginary = value;
+    }
 
-    ComplexNumber operator+(const ComplexNumber& other) const;
+    ComplexNumber<T> operator+(const ComplexNumber<T>& other) const
+    {
+        return ComplexNumber<T>(real + other.real, imaginary + other.imaginary);
+    }
 
-    ComplexNumber operator-(const ComplexNumber& other) const;
+    ComplexNumber<T> operator-(const ComplexNumber<T>& other) const
+    {
+        return ComplexNumber<T>(real - other.real, imaginary - other.imaginary);
+    }
 
-    ComplexNumber operator*(const ComplexNumber& other) const;
+    ComplexNumber operator*(const ComplexNumber& other) const
+    {
+        double newReal = (real * other.real) - (imaginary * other.imaginary);
+        double newImaginary = (real * other.imaginary) + (imaginary * other.real);
+        return ComplexNumber(newReal, newImaginary);
+    }
 
-    ComplexNumber operator/(const ComplexNumber& other) const;
+    ComplexNumber<T> operator/(const ComplexNumber<T>& other) const
+    {
+        if (other == 0)
+        {
+            throw std::logic_error("Division by zero.");
+        }
 
-    bool operator==(const ComplexNumber& other) const;
 
-    bool operator==(int value) const;
+        T denominator = (other.real * other.real) + (other.imaginary * other.imaginary);
+        T newReal = ((real * other.real) + (imaginary * other.imaginary)) / denominator;
+        T newImaginary = ((imaginary * other.real) - (real * other.imaginary)) / denominator;
+        return ComplexNumber<T>(newReal, newImaginary);
+    }
 
-    bool operator==(double value) const;
+    bool operator==(const ComplexNumber& other) const
+    {
+        return std::abs(real - other.real) < std::numeric_limits<double>::epsilon() &&
+            std::abs(imaginary - other.imaginary) < std::numeric_limits<double>::epsilon();
+    }
 
-    bool operator!=(const ComplexNumber& other) const;
+    bool operator==(int value) const
+    {
+        return std::abs(real - value) < std::numeric_limits<double>::epsilon() &&
+            std::abs(imaginary) < std::numeric_limits<double>::epsilon();
+    }
 
-    bool operator!=(int value) const;
+    bool operator==(double value) const
+    {
+        return std::abs(real - value) < std::numeric_limits<T>::epsilon() &&
+            std::abs(imaginary) < std::numeric_limits<T>::epsilon();
+    }
 
-    bool operator!=(float value) const;
+    bool operator!=(const ComplexNumber<T>& other) const
+    {
+        return !(*this == other);
+    }
 
-    ComplexNumber operator^(int exponent) const;
+    bool operator!=(T value) const
+    {
+        return !(*this == value);
+    }
 
-    double getModule() const;
+    ComplexNumber<T> operator^(int exponent) const
+    {
+        T r = std::pow(getModule(), exponent);
+        T angle = std::atan2(imaginary, real) * exponent;
+        return ComplexNumber<T>(r * std::cos(angle), r * std::sin(angle));
+    }
+
+    T getModule() const
+    {
+        return sqrt((real * real) + (imaginary * imaginary));
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const ComplexNumber<T>& complexNumber)
+    {
+        os << complexNumber.getReal();
+        if (complexNumber.getImaginary() >= 0)
+        {
+            os << " + " << complexNumber.getImaginary() << "i";
+        }
+        else
+        {
+            os << " - " << -complexNumber.getImaginary() << "i";
+        }
+        return os;
+    }
+
+    explicit operator int() const
+    {
+        return static_cast<int>(real);
+    }
+
+    explicit operator float() const
+    {
+        return static_cast<float>(real);
+    }
+
+    explicit operator std::string() const
+    {
+        std::string realStr = std::to_string(real);
+        std::string imagStr = std::to_string(abs(imaginary));
+
+        realStr.erase(realStr.find_last_not_of('0') + 1, std::string::npos);
+        imagStr.erase(imagStr.find_last_not_of('0') + 1, std::string::npos);
+
+        std::string result = '(' + realStr + (imaginary >= 0 ? " + " : " - ") + imagStr + "i" + ')';
+
+        return result;
+    }
 };
-
 
 #endif //COMPLEX_COMPLEXNUM_HPP
--- ComplexTest.cpp
+++ ComplexTest.cpp
@@ -5,54 +5,59 @@
 
 constexpr double epsilon = 1e-10;
 
+template <typename T>
 void test_complex_number_addition()
 {
-    ComplexNumber z1(1.0, 2.0);
-    ComplexNumber z2(3.0, 4.0);
-    ComplexNumber result = z1 + z2;
+    ComplexNumber<T> z1(1.0, 2.0);
+    ComplexNumber<T> z2(3.0, 4.0);
+    ComplexNumber<T> result = z1 + z2;
 
     assert(result.getReal() == 4.0);
     assert(result.getImaginary() == 6.0);
 }
 
+template <typename T>
 void test_complex_number_subtraction()
 {
-    ComplexNumber z1(5.0, 8.0);
-    ComplexNumber z2(2.0, 3.0);
-    ComplexNumber result = z1 - z2;
+    ComplexNumber<T> z1(5.0, 8.0);
+    ComplexNumber<T> z2(2.0, 3.0);
+    ComplexNumber<T> result = z1 - z2;
 
     assert(result.getReal() == 3.0);
     assert(result.getImaginary() == 5.0);
 }
 
+template <typename T>
 void test_complex_number_multiplication()
 {
-    ComplexNumber z1(2.0, 3.0);
-    ComplexNumber z2(4.0, 5.0);
-    ComplexNumber result = z1 * z2;
+    ComplexNumber<T> z1(2.0, 3.0);
+    ComplexNumber<T> z2(4.0, 5.0);
+    ComplexNumber<T> result = z1 * z2;
 
     assert(result.getReal() == -7.0);
     assert(result.getImaginary() == 22.0);
 }
 
+template <typename T>
 void test_complex_number_division()
 {
-    ComplexNumber z1(3.0, 4.0);
-    ComplexNumber z2(1.0, 2.0);
-    ComplexNumber result = z1 / z2;
+    ComplexNumber<T> z1(3.0, 4.0);
+    ComplexNumber<T> z2(1.0, 2.0);
+    ComplexNumber<T> result = z1 / z2;
 
     assert(result.getReal() == 2.2);
     assert(result.getImaginary() == -0.4);
 }
 
+template <typename T>
 void test_division_by_zero()
 {
-    ComplexNumber z1(3.0, 4.0);
-    ComplexNumber z2(0.0, 0.0);
+    ComplexNumber<T> z1(3.0, 4.0);
+    ComplexNumber<T> z2(0.0, 0.0);
 
     try
     {
-        ComplexNumber result = z1 / z2;
+        ComplexNumber<T> result = z1 / z2;
         assert(false && "Exception not thrown for division by zero.");
     }
     catch (const std::logic_error& e)
@@ -61,50 +66,88 @@ void test_division_by_zero()
     }
 }
 
-
+template <typename T>
 void test_complex_number_equality()
 {
-    ComplexNumber z1(1.0, 2.0);
-    ComplexNumber z2(1.0, 2.0);
+    ComplexNumber<T> z1(1.0, 2.0);
+    ComplexNumber<T> z2(1.0, 2.0);
     assert(z1 == z2);
 }
 
+template <typename T>
 void test_complex_number_inequality()
 {
-    ComplexNumber z1(1.0, 2.0);
-    ComplexNumber z2(3.0, 4.0);
+    ComplexNumber<T> z1(1.0, 2.0);
+    ComplexNumber<T> z2(3.0, 4.0);
     assert(z1 != z2);
-    assert(z1 != 3.0f);
+    assert(z1 != static_cast<T>(3.0));
 }
 
+template <typename T>
 void test_complex_number_power()
 {
-    ComplexNumber z(2.0, 3.0);
-    ComplexNumber result = z ^ 2.0;
+    ComplexNumber<T> z(2.0, 3.0);
+    ComplexNumber<T> result = z ^ 2;
 
     assert(result.getReal() - (-5.0) < epsilon);
     assert(result.getImaginary() - 12.0 < epsilon);
 }
 
+template <typename T>
 void test_complex_number_module()
 {
-    ComplexNumber z(3.0, 4.0);
+    ComplexNumber<T> z(3.0, 4.0);
     assert(z.getModule() - 5.0 < epsilon);
 }
 
+template <typename T>
+void test_cast_to_int()
+{
+    ComplexNumber<T> z(6.5, 1.7);
+    ComplexNumber<T> z1(2.5, -1.7);
+    int intValue = int(z);
+    int intValue2 = int(z1);
+    assert(intValue == 6);
+    assert(intValue2 == 2);
+}
+
+template <typename T>
+void test_cast_to_float()
+{
+    ComplexNumber<T> z(6.5, 1.7);
+    float floatValue = float(z);
+    ComplexNumber<T> z1(-3.5, -1.7);
+    float floatValue1 = float(z1);
+    assert(floatValue - 6.5 < epsilon);
+    assert(floatValue1 + 3.5 < epsilon);
+}
+
+template <typename T>
+void test_cast_to_string()
+{
+    ComplexNumber<T> z(6.5, 1.7);
+    std::string stringValue = std::string(z);
+    ComplexNumber<T> z1(6.5, -1.7);
+    std::string stringValue1 = std::string(z1);
+    assert(stringValue == "(6.5 + 1.7i)");
+    assert(stringValue1 == "(6.5 - 1.7i)");
+}
 
 int main()
 {
-    test_complex_number_addition();
-    test_complex_number_subtraction();
-    test_complex_number_multiplication();
-    test_complex_number_division();
-    test_division_by_zero();
-    test_complex_number_equality();
-    test_complex_number_inequality();
-    test_complex_number_power();
-    test_complex_number_module();
-
+    test_complex_number_addition<double>();
+    test_complex_number_subtraction<double>();
+    test_complex_number_multiplication<double>();
+    test_complex_number_division<double>();
+    test_division_by_zero<double>();
+    test_complex_number_equality<double>();
+    test_complex_number_inequality<double>();
+    test_complex_number_power<double>();
+    test_complex_number_module<double>();
+    test_cast_to_int<double>();
+    test_cast_to_float<double>();
+    test_cast_to_string<double>();
+    // static_assert(Arithmetic<std::string>); - ошибка компиляции, т.к string не арифметический
     std::cout << "All tests passed" << std::endl;
     return 0;
 }
